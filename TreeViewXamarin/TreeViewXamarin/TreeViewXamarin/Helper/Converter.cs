970b964a70334db64a2b4a927e543b1e897b70e1
lakshmi.natarajan <lakshmi.natarajan@syncfusion.com>
2021-03-07 23:59:56 +0530

    XAMARIN-38721 Sample added

---
+++ TreeViewXamarin/TreeViewXamarin/TreeViewXamarin/Helper/Converter.cs
@@ -0,0 +1,25 @@
+﻿using Syncfusion.TreeView.Engine;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TemplateSelector
+{
+    public class FileCountConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return null;
+            
+            var treeViewNode = value as TreeViewNode;
+            return "(" + treeViewNode.ChildNodes.Count + " files)";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TreeViewXamarin/TreeViewXamarin/TreeViewXamarin/Views/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Syncfusion.TreeView.Engine;
+using Syncfusion.XForms.TreeView;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TemplateSelector
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
